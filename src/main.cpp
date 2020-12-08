6bfa74c8e1d89612f1497a96b0164678608b6916
zsalamon <zsalamon#hotmail.com>
2020-12-08 23:52:52 +0100

    2nd commit

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,9 @@ void loop()
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
   // wait for a second
-  delay(200);
+  delay(1000);
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
    // wait for a second
-  delay(200);
+  delay(1000);
 }
